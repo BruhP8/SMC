0287ff66efb28a2342454431df8e9dafd9539e98
Leif Henriksen <leifhenriksen88@gmail.com>
2021-11-12 18:02:32 +0100

    change msg

--- TP3/TP3_etudiant/soft/main.c
+++ TP3/TP3_etudiant/soft/main.c
@@ -3,10 +3,11 @@
 __attribute__((constructor)) void  main()
 {
 	char  byte;
-
+	int i = 0;
 	while(1) 
         {
-	    tty_puts("\nhello world\n");
+            tty_printf("char count : %d\n, last char = %c\n", i, byte);
+	    i++;
 	    tty_getc(&byte);
 	}
 
